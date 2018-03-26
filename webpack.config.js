460c71d49211efa3fa2f6a9e38a119d53bf2c1ab
furashcka <furashcka@gmail.com>
2018-03-27 00:25:53 +0300

    testing

--- webpack.config.js
+++ webpack.config.js
@@ -2,14 +2,17 @@ const path = require('path');
 
 module.exports = {
     watch: true,
-    entry: './src/Feedback.js',
+    entry: {
+        'dist/Feedback.js': './src/Feedback.js',
+        'test/dist/unit.js': './test/unit/unit.js',
+    },
     output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'Feedback.js',
+        path: path.resolve(__dirname, ''),
+        filename: '[name]',
         library: 'Feedback',
         libraryTarget: 'umd'
     },
     resolve: {
-        modules: [ './src' ]
+        modules: [ './src', './test/unit' ]
     }
 };
