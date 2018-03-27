92b9680d4954c7d08cadce54afbaadd725bc886f
furashcka <furashcka@gmail.com>
2018-03-27 19:58:02 +0300

    code refactoring

--- test/unit/serialize.test.js
+++ test/unit/serialize.test.js
@@ -1,244 +1,199 @@
-var helper = require( 'helper' );
+var Form = require( 'util/Form' );
 var getInputsGroupedByName = require( 'getInputsGroupedByName' );
 var serialize = require( 'serialize' );
 
 module.exports = function() {
-    _inputTest({
+    _test(function( form ) {
+        form.addInput({
+            type: 'text',
+            name: 'name',
+            value: 'Evgeny Krylov'
+        });
+    })
+    .run({
         desc: 'input type text',
-        inputs: [
-            {
-                type: 'text',
-                name: 'name',
-                value: 'Evgeny Krylov'
-            }
-        ],
         toBe: 'name=Evgeny+Krylov'
     });
 
-    _inputTest({
+    _test(function( form ) {
+        form.addInput({
+            type: 'text',
+            name: 'name',
+            value: 'Evgeny Krylov',
+            disabled: true
+        });
+    })
+    .run({
         desc: 'disabled input type text',
-        inputs: [
-            {
-                type: 'text',
-                name: 'name',
-                value: 'Evgeny Krylov',
-                disabled: true
-            }
-        ],
         toBe: ''
     });
 
-    _inputTest({
-        desc: 'not checked input type radio',
-        inputs: [
-            {
+    _test(function( form ) {
+        form
+            .addInput({
                 type: 'radio',
                 name: 'sex',
                 value: 'female'
-            },
-            {
+            })
+            .addInput({
                 type: 'radio',
                 name: 'sex',
                 value: 'male'
-            }
-        ],
+            });
+    })
+    .run({
+        desc: 'not checked input type radio',
         toBe: ''
     });
 
-    _inputTest({
-        desc: 'checked input type radio',
-        inputs: [
-            {
+    _test(function( form ) {
+        form
+            .addInput({
                 type: 'radio',
                 name: 'sex',
                 value: 'female',
                 checked: true
-            },
-            {
+            })
+            .addInput({
                 type: 'radio',
                 name: 'sex',
                 value: 'male'
-            }
-        ],
+            });
+    })
+    .run({
+        desc: 'checked input type radio',
         toBe: 'sex=female'
     });
 
-    _inputTest({
-        desc: 'checked disabled input type radio',
-        inputs: [
-            {
+    _test(function( form ) {
+        form
+            .addInput({
                 type: 'radio',
                 name: 'sex',
                 value: 'female',
                 checked: true,
                 disabled: true
-            },
-            {
+            })
+            .addInput({
                 type: 'radio',
                 name: 'sex',
                 value: 'male',
+                checked: true,
                 disabled: true
-            }
-        ],
+            });
+    })
+    .run({
+        desc: 'checked disabled input type radio',
         toBe: ''
     });
 
-    _inputTest({
-        desc: 'checked 2 inputs type checkbox, must be array',
-        inputs: [
-            {
+    _test(function( form ) {
+        form
+            .addInput({
                 type: 'checkbox',
                 name: 'country[]',
                 value: 'United Kingdom',
                 checked: true
-            },
-            {
+            })
+            .addInput({
                 type: 'checkbox',
                 name: 'country[]',
                 value: 'Germany',
                 checked: true
-            }
-        ],
+            });
+    })
+    .run({
+        desc: 'checked 2 inputs type checkbox, must be array',
         toBe: 'country%5B%5D=United+Kingdom&country%5B%5D=Germany'
     });
 
-    _selectTest({
+    _test(function( form ) {
+        form.addSelect({
+            name: 'language'
+        })
+            .addOption({
+                text: 'php',
+                value: 'php'
+            })
+            .addOption({
+                text: 'javascript',
+                value: 'javascript',
+                selected: true
+            })
+            .addOption({
+                text: 'css',
+                value: 'css'
+            });
+    })
+    .run({
         desc: 'select-one test',
-        selects: [
-            {
-                attr: {
-                    name: 'language'
-                },
-                options: [
-                    {
-                        innerText: 'php',
-                        value: 'php'
-                    },
-                    {
-                        innerText: 'javascript',
-                        value: 'javascript',
-                        selected: true
-                    },
-                    {
-                        innerText: 'css',
-                        value: 'css'
-                    }
-                ]
-            }
-        ],
         toBe: 'language=javascript'
     });
 
-    _selectTest({
+    _test(function( form ) {
+        form.addSelect({
+            name: 'language',
+            disabled: true
+        })
+            .addOption({
+                text: 'php',
+                value: 'php'
+            })
+            .addOption({
+                text: 'javascript',
+                value: 'javascript',
+                selected: true
+            })
+            .addOption({
+                text: 'css',
+                value: 'css'
+            });
+    })
+    .run({
         desc: 'disabled select-one test',
-        selects: [
-            {
-                attr: {
-                    name: 'language',
-                    disabled: true
-                },
-                options: [
-                    {
-                        innerText: 'php',
-                        value: 'php'
-                    },
-                    {
-                        innerText: 'javascript',
-                        value: 'javascript',
-                        selected: true
-                    },
-                    {
-                        innerText: 'css',
-                        value: 'css'
-                    }
-                ]
-            }
-        ],
         toBe: ''
     });
 
-    _selectTest({
+    _test(function( form ) {
+        form.addSelect({
+            name: 'language[]',
+            multiple: true
+        })
+            .addOption({
+                text: 'php',
+                value: 'php',
+                selected: true
+            })
+            .addOption({
+                text: 'javascript',
+                value: 'javascript',
+                selected: true
+            })
+            .addOption({
+                text: 'css',
+                value: 'css'
+            });
+    })
+    .run({
         desc: 'select-multiple test',
-        selects: [
-            {
-                attr: {
-                    name: 'language[]',
-                    multiple: true
-                },
-                options: [
-                    {
-                        innerText: 'php',
-                        value: 'php',
-                        selected: true
-                    },
-                    {
-                        innerText: 'javascript',
-                        value: 'javascript',
-                        selected: true
-                    },
-                    {
-                        innerText: 'css',
-                        value: 'css'
-                    }
-                ]
-            }
-        ],
         toBe: 'language%5B%5D=php&language%5B%5D=javascript'
     });
 };
 
-function _inputTest( obj ) {
-    _test( obj, function( form ) {
-        helper.forEach( obj.inputs, function( inputAttr ) {
-            _addInput( form, inputAttr );
-        });
-    });
-}
-
-function _selectTest( obj ) {
-    _test( obj, function( form ) {
-        helper.forEach( obj.selects, function( select ) {
-            _addSelect( form, select.attr, select.options );
-        });
-    });
-}
-
-function _test( obj, fn ) {
-    it( obj.desc, function() {
-        var form = _createForm(function( form ) {
-            fn( form );
-        });
-        var res = serialize({
-            inputsGroupedByName: getInputsGroupedByName( form )
-        });
-
-        expect( res ).toBe( obj.toBe );
-    });
-}
-
-function _createForm( fn ) {
-    var $form = $( '<form />' );
-    var form = $form.get( 0 );
+function _test( fn ) {
+    var form = new Form();
 
     fn( form );
-    return form;
-}
 
-function _addInput( form, attr ) {
-    var $form = $( form );
-    var $input = $( '<input />', attr );
-
-    $form.append( $input );
-}
-
-function _addSelect( form, attr, optionsArr ) {
-    var $form = $( form );
-    var $select = $( '<select />', attr );
-
-    helper.forEach( optionsArr, function( optionAttr ) {
-        var $option = $( '<option />', optionAttr );
-        $select.append( $option );
+    var res = serialize({
+        inputsGroupedByName: getInputsGroupedByName( form.getFormEl() )
     });
 
-    $form.append( $select );
+    return {
+        run: function( obj ) {
+            it( obj.desc, function() {
+                expect( res ).toBe( obj.toBe );
+            });
+        }
+    };
 }
---
+++ test/unit/util/Form.js
@@ -0,0 +1,47 @@
+module.exports = function() {
+    this.lastAddedElement = null;
+    this.$form = $( '<form />', {
+        method: 'post',
+        action: 'https://httpbin.org/post'
+    });
+};
+
+module.exports.prototype.addInput = function( attr ) {
+    var self = this;
+    var $el = $( '<input />', attr );
+
+    this.$form.append( $el );
+
+    return {
+        addInput: function( attr ) {
+            self.addInput( attr );
+        }
+    };
+}
+
+module.exports.prototype.addSelect = function( attr ) {
+    var $el = $( '<select />', attr );
+    var obj = {
+        addOption: function( attr ) {
+            _addOption( $el, attr );
+            return obj;
+        }
+    };
+
+    this.$form.append( $el );
+
+    return obj;
+};
+
+module.exports.prototype.getFormEl = function() {
+    return this.$form.get( 0 );
+};
+
+module.exports.prototype.log = function() {
+    console.log( this.getFormEl() );
+};
+
+function _addOption( $parent, attr ) {
+    var $el = $( '<option />', attr );
+    $parent.append( $el );
+}
