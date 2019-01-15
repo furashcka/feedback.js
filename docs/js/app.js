c8c8fe822b113588ae9aace8a53ca27a0f37cc8e
furashcka <furashcka@gmail.com>
2019-01-15 16:42:03 +0300

    changed: modal -> fancybox & refactoring code

--- docs/js/app.js
+++ docs/js/app.js
@@ -5,181 +5,166 @@ $(function() {
         sidenav: $( '.sidenav' ),
         smothScrollContent: $( '[id]' ),
         dataParentSidenav: $( '[data-parent-sidenav]' ),
-        switchContent: $( '[data-content]' ),
-        triggerContent: $( '[data-trigger-content]' ),
-        exampleBtns: $( '[data-example]' ),
-        get modal() {
-            return $( '.modal' );
-        }
+        demonstrationBlock: $( '.demonstration-block' ),
+        exampleBtns: $( 'button[data-example]' )
     };
 
     $el.sidenav.html( $el.dataParentSidenav.html() );
     $el.sidenav.sidenav();
-
-    $el.triggerContent.on( 'click', function( e ) {
-        var $self = $( this );
-        var offsetTop = -70;
-        var scrollTop = $self.offset().top + offsetTop;
-        var el = $self.data( 'trigger-content' );
-
-        $el.switchContent.hide();
-        $( '[data-content="' + el + '"]' ).show();
-
-        setTimeout(function() {
-            $( 'html, body' ).scrollTop( scrollTop );
-        });
+    $el.smothScrollContent.scrollSpy({
+        scrollOffset: 80
     });
+    app.initDemonstrationBlock( $el.demonstrationBlock, 'html' );
 
-    $el.exampleBtns.on( 'click', function( e ) {
-        var $self = $( this );
-        var src = 'examples/' + $self.data( 'example' ) + '/';
-        var html = [
-            '<button class="waves-effect waves-light btn-small" data-language="markup" data-src="' + src + 'index.html">html</button> ',
-            '<button class="waves-effect waves-light btn-small" data-language="css" data-src="' + src + 'app.css">ccs</button> ',
-            '<button class="waves-effect waves-light btn-small" data-language="javascript" data-src="' + src + 'app.js">js</button> ',
-            '<button class="waves-effect waves-light btn-small btn-close">result</button>',
-            '<div class="modal-code"></div>',
-            '<iframe src="' + src + '"></iframe>'
-        ].join( '' );
+    $el.exampleBtns.on( 'click', function() {
+        var src = 'examples/' + $( this ).data( 'example' ) + '/';
+        var html = $( '#demonstration-block-template' ).html().replace( /{{src}}/g, src );
 
         app.modal({
-            title: 'Demo - ' + $self.text(),
             html: html,
-            before: function() {
-                var $modal = app.modal.getEl();
-                var $iframe = $modal.find( 'iframe' );
-                var $btn = $modal.find( 'button[data-src]' );
-                var $btnClose = $modal.find( '.btn-close' );
-                var $modalCode = $modal.find( '.modal-code' );
+            after: function() {
+                var $demonstrationBlock = app.modal.getEl().find( '.demonstration-block' );
 
-                $btn.on( 'click', function() {
-                    var $self = $( this );
-                    var src = $self.attr( 'data-src' );
-                    var code = $self.data( 'language' );
-                    var className = 'language-' + code;
-
-                    $modalCode.html( '<div class="progress"><div class="indeterminate"></div></div>' );
-
-                    $.ajax({
-                        url: src,
-                        dataType: 'text',
-                        success: function( code ) {
-                            code = _escapeHtml( code );
-
-                            $btnClose.show();
-                            $iframe.hide();
-                            $modalCode.html( '<pre><code class="' + className + '">\n' + code + '\n\n</code></pre>' );
-                            Prism.highlightElement( $modal.find( 'code' ).get( 0 ) );
-                        }
-                    });
-                });
-
-                $btnClose.on( 'click', function() {
-                    $modalCode.html( '' );
-                    $btnClose.hide();
-                    $iframe.show();
-                });
-
-                $iframe.on( 'load', function() {
-                    var $body = $iframe.contents().find( 'body' );
-                    
-                    $body.on( 'click', function() {
-                        _update( $body );
-                    });
-
-                    _update( $body );
-                });
-
-                function _update( $body ) {
-                    setTimeout(function() {
-                        var height = $body.get( 0 ).scrollHeight;
-
-                        $iframe.height( height );
-                    }, 100);
-                }
-
-                function _escapeHtml( html ) {
-                    return html
-                         .replace( /&/g, '&amp;' )
-                         .replace( /</g, '&lt;' )
-                         .replace( />/g, '&gt;' )
-                         .replace( /"/g, '&quot;' )
-                         .replace( /'/g, '&#039;' );
-                 }
+                app.initDemonstrationBlock( $demonstrationBlock );
             }
         });
-
-        e.preventDefault();
     });
 
     $el.body.on( 'feedback.response', function() {
         var responseText = arguments[ 1 ].xhr.responseText;
-        var html = '<pre><code class="language-json">' + responseText + '</code></pre>';
+        var html = [
+            '<h4>Response from httpbin.org</h4>',
+            '<pre>',
+                '<code class="language-json">\n',
+                    responseText,
+                '\n</code>',
+            '</pre>'
+        ].join( '' );
 
         app.modal({
-            id: 'modal-console',
-            title: 'Response from httpbin.org',
             html: html,
             after: function() {
+                var $code = app.modal.getEl().find( 'code' ).get( 0 );
+
+                Prism.highlightElement( $code );
                 console.log( responseText );
-                Prism.highlightElement( app.modal.getEl().find( 'code' ).get( 0 ) );
             }
         });
-    });
-
-    $el.smothScrollContent.scrollSpy({
-        scrollOffset: 80
     });
 });
 
 (function() {
     var app = window.app = window.app || {};
     var $el = {
-        body: $( 'body' ),
-        get modal() {
-            return $( '.modal' );
-        },
-        get modalOverlay() {
-            return $( '.modal-overlay' )
-        }
+        body: $( 'body' )
     };
 
-    app.modal = function() {
-        var options = arguments[ 0 ] || {};
-        var opts = {
-            id: options.id || 'simple-modal',
-            title: options.title || '',
-            html: options.html || '',
-            before: options.before || function() {},
-            after: options.after || function() {}
-        };
-        var html = [
-            '<div id="{{id}}" class="modal modal-fixed-footer">',
-            '    <div class="modal-content">',
-            '        <h4>{{title}}</h4>',
-            '        {{html}}',
-            '    </div>',
-            '    <div class="modal-footer">',
-            '        <a href="#!" class="modal-close waves-effect waves-green btn-flat">Close</a>',
-            '    </div>',
-            '</div>',
-        ].join( '' );
-
-        html = html.replace( '{{id}}', opts.id );
-        html = html.replace( '{{title}}', opts.title );
-        html = html.replace( '{{html}}', opts.html );
-
-        $el.modal.modal( 'close' );
-        $el.modal.remove();
-        $el.body.append( html );
-
-        $el.modal.modal({
-            onOpenStart: opts.before,
-            onOpenEnd: opts.after
-        }).modal( 'open' );
+    app.modal = function( opts ) {
+        $.fancybox.close();
+        $.fancybox.open({
+            src: '<div>' + opts.html + '</div>',
+            type: 'html',
+            smallBtn: false,
+            touch: {
+                vertical: false,
+                momentum: false
+            },
+            afterShow: opts.after
+        });
     };
 
     app.modal.getEl = function() {
-        return $el.modal;
+        return $( '.fancybox-inner' ).eq( 0 );
+    };
+
+    app.initDemonstrationBlock = function( $elBlock, showDefault ) {
+        var $el = {
+            btn: $elBlock.find( 'button' ),
+            preloader: $elBlock.find( '.demonstration-block__preloader' ),
+            code: $elBlock.find( '.demonstration-block__code' ),
+            iframe: $elBlock.find( '.demonstration-block__iframe iframe' )
+        };
+
+        $el.iframe.on( 'load', function() {
+            var $body = $el.iframe.contents().find( 'body' );
+            var cache = 0;
+
+            setInterval( function() {
+                var height = $body.get( 0 ).scrollHeight;
+
+                if( height !== cache ) {
+                    cache = height;
+
+                    $el.iframe.height( height );
+                }
+            }, 200 );
+        });
+
+        $el.btn.on( 'click', function() {
+            var text = this.innerText.toLowerCase();
+            var src = this.getAttribute( 'data-src' );
+            var lang = '';
+
+            if( text === 'result' ) {
+                $el.code.hide();
+                $el.iframe.show();
+                return;
+            }
+
+            switch( text ) {
+                case 'html':
+                    lang = 'language-markup';
+                    break;
+                case 'css':
+                    lang = 'language-css';
+                    break;
+                case 'js':
+                    lang = 'language-javascript';
+                    break;
+            }
+
+            $el.preloader.show();
+            $.ajax({
+                url: src,
+                dataType: 'text',
+                success: function( code ) {
+                    var html = '';
+
+                    code = _escapeHtml( code );
+                    html = '<pre><code class="' + lang + '">\n' + code + '\n\n</code></pre>';
+
+                    $el.code.html( html );
+                    $el.code.show();
+                    $el.iframe.hide();
+                    $el.preloader.hide();
+                    Prism.highlightElement( $el.code.find( 'code' ).get( 0 ) );
+                }
+            });
+        });
+
+        if( showDefault ) {
+            switch( showDefault ) {
+                case 'html':
+                    $elBlock.find( 'button[data-src$="html"]' ).trigger( 'click' );
+                    break;
+
+                case 'css':
+                    $elBlock.find( 'button[data-src$="css"]' ).trigger( 'click' );
+                    break;
+
+                case 'js':
+                    $elBlock.find( 'button[data-src$="js"]' ).trigger( 'click' );
+                    break;
+            }
+        }
+
+        function _escapeHtml( html ) {
+            return html
+                 .replace( /&/g, '&amp;' )
+                 .replace( /</g, '&lt;' )
+                 .replace( />/g, '&gt;' )
+                 .replace( /"/g, '&quot;' )
+                 .replace( /'/g, '&#039;' );
+         }
     };
 })();
